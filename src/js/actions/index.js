096a1a54591f18adb7f1e1d977b56fbe5aa413fa
asherjames <asherjames.nz@gmail.com>
2016-06-29 21:30:47 +1200

    Finish action creators for genres

--- src/js/actions/index.js
+++ src/js/actions/index.js
@@ -1,26 +1,38 @@
 import axios from 'axios'
 
-export const REQUEST_GENRES = 'REQUEST_GENRES'
+export const FETCH_GENRES = 'FETCH_GENRES'
+export const FETCH_GENRES_ERROR = 'FETCH_GENRES_ERROR'
 export const RECEIVE_GENRES = 'RECEIVE_GENRES'
 
-export function requestGenres() {
+export function fetchGenres() {
 	return {
-		type: REQUEST_GENRES
+		type: FETCH_GENRES
 	}
 }
 
 export function receiveGenres(json) {
 	return {
 		type: RECEIVE_GENRES,
-		genres: json
+		payload: json
+	}
+}
+
+export function fetchGenresError(err) {
+	return {
+		type: FETCH_GENRES_ERROR,
+		payload: err
 	}
 }
 
 export function fetchGenres() {
-	return dispatch => {
-		dispatch(requestGenres())
-		return fetch('localhost:3000/filim-api/genres')
-			.then(response => response.json())
-			.then(json => dispatch(receiveGenres(json)))
+	return (dispatch) => {
+		dispatch(fetchGenres())
+		axios.get("http://localhost:3000/filim-api/genres")
+			.then((response) => {
+				dispatch({receiveGenres(response.data)})
+			})
+			.catch((err) => {
+				dispatch({fetchGenresError(err)})
+			})
 	}
 }
