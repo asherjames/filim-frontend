85998672e80646844d513a35f4c6934610b667f0
asherjames <asherjames.nz@gmail.com>
2016-07-17 17:06:35 +1200

    Add ReleaseYearSelector presentational component

---
+++ src/js/components/ReleaseYearSelector.js
@@ -0,0 +1,24 @@
+import React, {PropTypes} from 'react'
+import DropDownMenu from 'material-ui/DropDownMenu'
+import MenuItem from 'material-ui/MenuItem'
+
+const ReleaseYearSelector = ({years, onFromYearClick, onToYearClick}) => (
+    <DropDownMenu
+        onChange = {(event, key, payload) => onFromYearClick(payload)}>
+        {years.map(year =>
+            <MenuItem value={year} key={year} primaryText={year}/>
+        )}
+    </DropDownMenu>
+    <DropDownMenu
+        onChange = {(event, key, payload) => onToYearClick(payload)}>
+        {years.map(year =>
+            <MenuItem value={year} key={year} primaryText={year}/>
+        )}
+    </DropDownMenu>
+)
+
+ReleaseYearSelector.propTypes = {
+    years: PropTypes.arrayOf(PropTypes.number).isRequired,
+    onFromYearClick: PropTypes.func.isRequired,
+    onToYearClick: PropTypes.func.isRequired
+}
