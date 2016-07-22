150f10f0485df0005facc0326b24bc50efba334d
Asher James <asherjames.nz@gmail.com>
2016-07-22 14:09:55 +1200

    Add actions for release year component

--- src/js/actions/actions.js
+++ src/js/actions/actions.js
@@ -4,7 +4,9 @@ export const FETCH_GENRES = 'FETCH_GENRES'
 export const FETCH_GENRES_ERROR = 'FETCH_GENRES_ERROR'
 export const RECEIVE_GENRES = 'RECEIVE_GENRES'
 export const TOGGLE_TV_MOVIE = 'TOGGLE_TV_MOVIE'
-export const GENRE_SELECTED = "GENRE_SELECTED"
+export const GENRE_SELECTED = 'GENRE_SELECTED'
+export const TO_RELEASE_CHANGED = 'TO_RELEASE_CHANGED'
+export const FROM_RELEASE_CHANGED = 'FROM_RELEASE_CHANGED'
 
 export function beginFetchGenres() {
     return {
@@ -50,5 +52,19 @@ export function genreSelected(id) {
     return {
         type: GENRE_SELECTED,
         payload: id
+    }
+}
+
+export function toReleaseYearChanged(year) {
+    return {
+        type: TO_RELEASE_CHANGED,
+        payload: year
+    }
+}
+
+export function fromReleaseYearChanged(year) {
+    return {
+        type: FROM_RELEASE_CHANGED,
+        payload: year
     }
 }
