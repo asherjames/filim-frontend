fe590e243a382aa139adaf2408663761d18245fa
asherjames <asherjames.nz@gmail.com>
2016-07-26 20:58:02 +1200

    Move async reducer code into separate function

---
+++ src/js/reducers/AsyncReducer.js
@@ -0,0 +1,24 @@
+import {BEGIN_FETCH_GENRES,
+        FETCH_GENRES,
+        FETCH_GENRES_ERROR,
+        RECEIVE_GENRES} from '../actions/asyncActions'
+
+export default function reducer(state, action) {
+    switch(action.type) {
+        case RECEIVE_GENRES:
+            return Object.assign({}, state, {
+                genres: action.payload, 
+                isFetchingGenres: false
+            })
+        case FETCH_GENRES:
+            return Object.assign({}, state, {
+                isFetchingGenres: true
+            })
+        case FETCH_GENRES_ERROR:
+            return Object.assign({}, state, {
+                isFetchingGenres: false
+            })
+        default:
+            return state
+    }
+}
--- src/js/reducers/Reducer.js
+++ src/js/reducers/Reducer.js
@@ -1,36 +1,24 @@
-import { combineReducers } from 'redux'
-import * as actType from '../actions/actions'
-import * as asyncType from '../actions/asyncActions'
+import {TOGGLE_TV_MOVIE,
+        GENRE_SELECTED,
+        TO_RELEASE_CHANGED,
+        FROM_RELEASE_CHANGED} from '../actions/actions'
 
 export default function reducer(state, action) {
     switch (action.type) {
-        case asyncType.RECEIVE_GENRES:
-            console.log('received genres: ' + action.payload)
-            return Object.assign({}, state, {
-                genres: action.payload, 
-                isFetchingGenres: false
-            })
-        case actType.FETCH_GENRES:
-            return Object.assign({}, state, {
-                isFetchingGenres: true
-            })
-        case actType.FETCH_GENRES_ERROR:
-            return Object.assign({}, state, {
-                isFetchingGenres: false
-            })
-        case actType.TOGGLE_TV_MOVIE:
+
+        case TOGGLE_TV_MOVIE:
             return Object.assign({}, state, {
                 tvOrMovie: action.payload
             })
-        case actType.GENRE_SELECTED:
+        case GENRE_SELECTED:
             return Object.assign({}, state, {
                 selectedGenre: action.payload
             })
-        case actType.TO_RELEASE_CHANGED:
+        case TO_RELEASE_CHANGED:
             return Object.assign({}, state, {
                 toReleaseYear: action.payload
             })
-        case actType.FROM_RELEASE_CHANGED:
+        case FROM_RELEASE_CHANGED:
             return Object.assign({}, state, {
                 fromReleaseYear: action.payload
             })
