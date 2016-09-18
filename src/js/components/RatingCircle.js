57e10bb5a7aaaab2583e9c3fb44bab37e3eac776
asherjames <asherjames.nz@gmail.com>
2016-09-18 18:39:29 +1200

    Add conditional colours and render method to RatingCircle

--- src/js/components/RatingCircle.js
+++ src/js/components/RatingCircle.js
@@ -5,13 +5,32 @@ export default class RatingCircle extends React.Component {
 
     constructor(props) {
         super(props)
-        this.state = {
-            colour: "00D438"
+        this.colour = ""
+    }
+
+    componentWillMount() {
+        if (this.props.rating < 5.0) {
+            this.colour = "#FF432E"
+        } else if (this.props.rating < 6.0 && this.props.rating >= 5.0) {
+            this.colour = "#FFAC00"
+        } else if (this.props.rating < 7.0 && this.props.rating >= 6.0) {
+            this.colour = "#FFEB00"
+        } else if (this.props.rating < 8.0 && this.props.rating >= 7.0) {
+            this.colour = "#DBFF00"
+        } else if (this.props.rating < 9.0 && this.props.rating >= 8.0) {
+            this.colour = "#1AFF00"
+        } else if (this.props.rating >= 9.0) {
+            this.colour = "#00D2FF"
         }
     }
 
-    componentDidMount() {
-
+    render() {
+        return(
+            <div>
+                <span>{this.props.rating}</span>
+                <CircularProgress value={this.props.rating} min={0.0} max={10.0} color={this.colour}/>
+            </div>
+        )
     }
 }
 
