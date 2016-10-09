a3b048b9717d60545ee4243be8110be4d8d35f66
asherjames <asherjames.nz@gmail.com>
2016-10-09 17:14:44 +1300

    Put array of sorters inside SortByDropDown, generate icons according to array

--- src/components/SortByDropdown.js
+++ src/components/SortByDropdown.js
@@ -1,6 +1,8 @@
 import React, {PropTypes} from 'react'
 import DropDownMenu from 'material-ui/DropDownMenu'
 import MenuItem from 'material-ui/MenuItem'
+import NavigationArrowUpward from 'material-ui/svg-icons/navigation/arrow-upward'
+import NavigationArrowDownward from 'material-ui/svg-icons/navigation/arrow-downward'
 
 
 export default class SortByDropdown extends React.Component {
@@ -8,6 +10,58 @@ export default class SortByDropdown extends React.Component {
     constructor(props) {
         super(props)
         this.state = {sorter: ''}
+        this.sorters = [
+            {
+                id: 'popularity.asc',
+                name: 'Popularity',
+                asc: true
+            },
+            {
+                id: 'popularity.desc',
+                name: 'Popularity',
+                asc: false
+            },
+            {
+                id: 'release_date.asc',
+                name: 'Release date',
+                asc: true
+            },
+            {
+                id: 'release_date.desc',
+                name: 'Release date',
+                asc: false
+            },
+            {
+                id: 'original_title.asc',
+                name: 'Title',
+                asc: true
+            },
+            {
+                id: 'original_title.desc',
+                name: 'Title',
+                asc: false
+            },
+            {
+                id: 'vote_average.asc',
+                name: 'Rating',
+                asc: true
+            },
+            {
+                id: 'vote_average.desc',
+                name: 'Rating',
+                asc: false
+            },
+            {
+                id: 'vote_count.asc',
+                name: 'Number of votes',
+                asc: true
+            },
+            {
+                id: 'vote_count.desc',
+                name: 'Number of votes',
+                asc: true
+            }
+        ]
     }
 
     handleChange(event, index, value) {
@@ -19,11 +73,11 @@ export default class SortByDropdown extends React.Component {
         return (
             <DropDownMenu onChange={this.handleChange} value={this.state.sorter}>
                 <MenuItem value={0} key={0} primaryText="Sort by..." disabled={true}/>
-                {this.props.sorters.map(sorter =>
+                {this.sorters.map(sorter =>
                     <MenuItem value={sorter.id}
                               key={sorter.id}
                               primaryText={sorter.name}
-                              leftIcon={sorter.icon ? sorter.icon : ''}
+                              leftIcon={sorter.asc ? <NavigationArrowUpward/> : <NavigationArrowDownward/>}
                     />
                 )}
             </DropDownMenu>
@@ -32,10 +86,5 @@ export default class SortByDropdown extends React.Component {
 }
 
 SortByDropdown.propTypes = {
-    sorters: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.string.isRequired,
-        name: PropTypes.string.isRequired,
-        icon: PropTypes.string
-    }).isRequired).isRequired,
     onSorterClick: PropTypes.func.isRequired
 }
