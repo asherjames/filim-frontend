2f207cfe892e80550ef257365fb66992ad4addb1
asherjames <asherjames.nz@gmail.com>
2016-10-10 22:29:53 +1300

    add travis

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
         ]
     },
     output: {
-        path: __dirname + "/src/",
+        path: __dirname + "/dist/",
         filename: "script.min.js"
     },
     plugins: debug ? [] : [
