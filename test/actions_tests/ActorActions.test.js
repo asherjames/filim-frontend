90fe0952fa9269ff01ff4246ea8d6e698b804aea
asherjames <asherjames.nz@gmail.com>
2016-10-14 20:55:06 +1300

    create ActorActions unit tests

---
+++ test/actions_tests/ActorActions.test.js
@@ -0,0 +1,26 @@
+import * as actions from '../../src/actions/ActorActions'
+
+describe('Actor actions', () => {
+    const actor = {
+        name: 'Sean Connery',
+        id: 123
+    }
+
+    it('should create an action to add an actor', () => {
+        const expectedAction = {
+            type: actions.ACTOR_SELECTED,
+            payload: actor
+        }
+
+        expect(actions.actorSelected(actor)).toEqual(expectedAction)
+    })
+
+    it('should create an action to delete an actor', () => {
+        const expectedAction = {
+            type: actions.ACTOR_DELETED,
+            payload: actor
+        }
+
+        expect(actions.actorDeleted(actor)).toEqual(expectedAction)
+    })
+})
